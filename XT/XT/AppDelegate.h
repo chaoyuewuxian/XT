2844d6a069e96cb0505697a3bbe58e8705e62b72
张瑞超 <359599907@qq.com>
2016-11-30 22:27:58 +0800

    上传第一份文件

---
+++ XT/XT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XT
+//
+//  Created by 张瑞超 on 2016/11/30.
+//  Copyright © 2016年 张瑞超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XT/XT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XT
+//
+//  Created by 张瑞超 on 2016/11/30.
+//  Copyright © 2016年 张瑞超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
